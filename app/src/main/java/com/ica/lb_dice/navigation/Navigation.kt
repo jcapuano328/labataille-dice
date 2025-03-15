20d14835d6aefec22c0f41422613a4099e4959ab
Jeff Capuano <jcapuano328@gmail.com>
2025-03-15 14:17:28 -0400

    checkpoint 3

--- app/src/main/java/com/ica/lb_dice/navigation/Navigation.kt
+++ app/src/main/java/com/ica/lb_dice/navigation/Navigation.kt
@@ -1,22 +1,32 @@
 package com.ica.lb_dice.navigation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.compose.material3.FabPosition
+import androidx.compose.foundation.layout.RowScope
+import androidx.navigation.NavDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+
+import com.ica.lb_dice.R
 import com.ica.lb_dice.ui.FontAwesomeIcon
 import com.ica.lb_dice.screens.FireCombatScreen
 import com.ica.lb_dice.screens.MeleeCombatScreen
@@ -30,18 +40,25 @@ fun MainNavigation() {
     val navController = rememberNavController()
 
     Scaffold(
-        bottomBar = { MainBottomNavigationBar(navController = navController) } // Pass navController here
+        bottomBar = { MainBottomNavigationBar(navController = navController) },
+        floatingActionButton = { MainFloatingActionButton(navController) },
+        floatingActionButtonPosition = FabPosition.Center
     ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = NavigationDestinations.FireCombat.route,
-            modifier = Modifier.padding(innerPadding) // Apply padding here
-        ) {
-            composable(NavigationDestinations.FireCombat.route) { FireCombatScreen(navController) }
-            composable(NavigationDestinations.MeleeCombat.route) { MeleeCombatScreen(navController) }
-            composable(NavigationDestinations.MoraleCheck.route) { MoraleCheckScreen(navController) }
-            composable(NavigationDestinations.General.route) { GeneralScreen(navController) }
-        }
+        MainNavigationContent(navController, innerPadding)
+    }
+}
+
+@Composable
+fun MainNavigationContent(navController: NavHostController, innerPadding: PaddingValues) {
+    NavHost(
+        navController = navController,
+        startDestination = NavigationDestinations.FireCombat.route,
+        modifier = Modifier.padding(innerPadding) // Apply padding here
+    ) {
+        composable(NavigationDestinations.FireCombat.route) { FireCombatScreen(navController) }
+        composable(NavigationDestinations.MeleeCombat.route) { MeleeCombatScreen(navController) }
+        composable(NavigationDestinations.MoraleCheck.route) { MoraleCheckScreen(navController) }
+        composable(NavigationDestinations.General.route) { GeneralScreen(navController) }
     }
 }
 
@@ -50,30 +67,55 @@ fun MainBottomNavigationBar(navController: NavHostController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
-    NavigationBar { // or BottomNavigation
-        listOf(
-            NavigationDestinations.FireCombat,
-            NavigationDestinations.MeleeCombat,
-            NavigationDestinations.MoraleCheck,
-            NavigationDestinations.General
-        ).forEach { destination ->
-            NavigationBarItem( // or BottomNavigationItem
-                icon = { FontAwesomeIcon(unicode = destination.icon.toString(), description = destination.route, modifier = Modifier.padding(20.dp)) }, // Replace with appropriate icons
-                label = { Text(text = destination.route) },
-                selected = currentDestination?.hierarchy?.any { it.route == destination.route } == true,
-                onClick = {
-                    navController.navigate(destination.route) {
-                        // Avoid building up a large stack of destinations
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        // Avoid multiple copies of the same destination when reselecting the same item
-                        launchSingleTop = true
-                        // Restore state when reselecting a previously selected item
-                        restoreState = true
+    BottomAppBar(
+        actions = {
+            MyNavigationItems(navController, currentDestination)
+        },
+        modifier = Modifier,
+        tonalElevation = 10.dp,
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+    )
+}
+
+@Composable
+fun RowScope.MyNavigationItems(navController: NavHostController, currentDestination: NavDestination?) {
+    listOf(
+        NavigationDestinations.FireCombat,
+        NavigationDestinations.MeleeCombat,
+        NavigationDestinations.MoraleCheck,
+        NavigationDestinations.General
+    ).forEach { destination ->
+        NavigationBarItem(
+            icon = { FontAwesomeIcon(unicode = destination.icon, description = destination.route) },
+            label = { Text(text = destination.route) },
+            selected = currentDestination?.hierarchy?.any { it.route == destination.route } == true,
+            onClick = {
+                navController.navigate(destination.route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
                     }
+                    launchSingleTop = true
+                    restoreState = true
                 }
+            },
+            colors = NavigationBarItemDefaults.colors(
+                indicatorColor = Color.Transparent,
+                selectedIconColor = Color.White,
+                selectedTextColor = Color.Red,
+                unselectedIconColor = Color.Gray,
+                unselectedTextColor = Color.Gray
             )
-        }
+        )
+    }
+}
+
+
+@Composable
+fun MainFloatingActionButton(navController: NavHostController) {
+    FloatingActionButton(onClick = { /*TODO*/ }) {
+        Image(
+            painter = painterResource(id = R.drawable.dice),
+            contentDescription = "Roll"
+        )
     }
 }
