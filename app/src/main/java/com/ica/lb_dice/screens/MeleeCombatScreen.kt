4cf606b0193095a9cae6e77149a3633454b0cfed
Jeff Capuano <jcapuano328@gmail.com>
2025-04-27 08:35:05 -0400

    morale check

--- app/src/main/java/com/ica/lb_dice/screens/MeleeCombatScreen.kt
+++ app/src/main/java/com/ica/lb_dice/screens/MeleeCombatScreen.kt
@@ -1,12 +1,10 @@
 package com.ica.lb_dice.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -16,7 +14,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/ica/lb_dice/viewmodels/MoraleCheckViewModel.kt
+++ app/src/main/java/com/ica/lb_dice/viewmodels/MoraleCheckViewModel.kt
@@ -1,7 +1,13 @@
 package com.ica.lb_dice.viewmodels
 
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ica.lb_dice.services.FireCombatService
+import com.ica.lb_dice.services.LeaderCasualtyService
+import com.ica.lb_dice.services.MoraleService
+import com.ica.lb_dice.util.DiceBase6
+import com.ica.lb_dice.util.DieConfig
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -9,19 +15,72 @@ import kotlinx.coroutines.launch
 import com.ica.lb_dice.util.MathUtils
 
 class MoraleCheckViewModel : ViewModel() {
-    private val _randomNumbers = MutableStateFlow<List<Int>>(emptyList())
-    val randomNumbers = _randomNumbers.asStateFlow()
+    private val _diceSetMorale = MutableStateFlow(DiceSet(emptyList(), MutableStateFlow(emptyList())))
+    val diceSetMorale = _diceSetMorale.asStateFlow()
 
-    private fun generateRandomNumbers() {
+    private val _resultsSet = MutableStateFlow(FireCombatResultsSet(0, emptyList(), 0, 0, LeaderCasualtyResult("", ""), 0, emptyList()))
+    val resultsSet = _resultsSet.asStateFlow()
+
+    init {
+        createDiceSets()
+        setInitialDieValues()
+        updateResults()
+    }
+
+    private fun createDiceSets() {
+        _diceSetMorale.value = DiceSet(listOf(DieConfig(backgroundColor = Color.Black, dotColor = Color.White), DieConfig(backgroundColor = Color.Black, dotColor = Color.Red)), MutableStateFlow(emptyList()))
+    }
+    private fun setInitialDieValues() {
         viewModelScope.launch {
-            val random = MathUtils()
-            // Generate 2 random numbers from 1 to 6
-            val newNumbers = List(2) { random.randomDie6() }
-            _randomNumbers.value = newNumbers
+            _diceSetMorale.value.dieValues.value = List(_diceSetMorale.value.dieConfigs.size) { 1 }
+        }
+    }
+
+    fun incrementMoraleDie(die: Int) {
+        viewModelScope.launch {
+            var newValue = _diceSetMorale.value.dieValues.value[die] + 1
+            if (newValue > 6) newValue = 1
+            _diceSetMorale.value.dieValues.value = _diceSetMorale.value.dieValues.value.toMutableList().also { it[die] = newValue }
+            updateResults()
+        }
+    }
+
+    fun modifyMoraleDice(value: Int) {
+        viewModelScope.launch {
+            val newValue = DiceBase6.fromDice(
+                _diceSetMorale.value.dieValues.value[0],
+                _diceSetMorale.value.dieValues.value[1]
+            ).plus(value)
+            println("FireCombatViewModel.modifyMoraleDice() New Value: ${newValue.toDiceBase6()}")
+            _diceSetMorale.value.dieValues.value = newValue.decompose().toList()
+            updateResults()
         }
     }
 
     suspend fun onFabClicked() {
-        generateRandomNumbers()
+        viewModelScope.launch {
+            rollDice()
+            updateResults()
+        }
+    }
+
+    private fun rollDice() {
+        println("MoraleCheckViewModel: roll dice:")
+        val random = MathUtils()
+
+        // Generate random numbers from 1 to 6
+        _diceSetMorale.value.dieValues.value = List(_diceSetMorale.value.dieConfigs.size) { random.randomDie6() }
+        println("Set Morale: ${_diceSetMorale.value.dieValues.value}")
+    }
+
+    private fun updateResults() {
+        updateMoraleResults()
+    }
+
+    private fun updateMoraleResults() {
+        println("MoraleCheckViewModel: updateMoraleResults:")
+        val moraleDice = DiceBase6.fromDice(_diceSetMorale.value.dieValues.value[0], _diceSetMorale.value.dieValues.value[1]).toDiceBase6()
+        val moraleResults = MoraleService().check(moraleDice)
+        _resultsSet.value = _resultsSet.value.copy(moraleResults = moraleResults.map { MoraleResult(it.result, it.modifier, it.icon) })
     }
 }
